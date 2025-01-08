717116599477c0cabce3aeb5dee7b034ee8ae610
Mark Piper <mark.piper@colorado.edu>
2025-01-08 21:06:27 +0000

    Remove extraneous statements

--- diffusion-model.py
+++ diffusion-model.py
@@ -9,16 +9,6 @@ dx = 0.5
 x = np.arange(start=0, stop=Lx, step=dx)
 nx = len(x)
 
-whos
-
-x[0]
-
-x[nx-1]
-
-x[-1]
-
-x[0:5]
-
 C = np.zeros_like(x)
 C_left = 500
 C_right = 0
@@ -35,16 +25,6 @@ time = 0
 nt = 5000
 dt = 0.5 * (dx**2 / D)
 
-dt
-
-z = 5
-
-z = z + 5
-z
-
-z += 5
-z
-
 for t in range(0, nt):
     C += D * dt / dx**2 * (np.roll(C, -1) - 2*C + np.roll(C, 1))
     C[0] = C_left
@@ -55,15 +35,3 @@ plt.plot(x, C, "b")
 plt.xlabel("x")
 plt.ylabel("C")
 plt.title("Final concentration profile")
-
-z = np.arange(5)
-z
-
-np.roll(z, -1)
-
-np.roll(z, 1)
-
-z + 1
-
-for i in range(len(z)):
-    print(z[i] + 1)
