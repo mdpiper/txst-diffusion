5cf1631364ca3415a51762678052c1f74d33dff7
Mark Piper <mark.piper@colorado.edu>
2025-01-09 17:40:41 +0000

    Set up sample main program

--- diffusion.py
+++ diffusion.py
@@ -31,22 +31,33 @@ def solve_1d_diffusion(concentration, grid_spacing=1.0, time_step=1.0, diffusivi
     concentration[1:-1] += diffusivity * time_step / grid_spacing**2 * centered_difference[1:-1]
 
 
-def diffusion_model():
+def diffusion_model(plot=False, domain_size=300, n_time_steps=5000):
     D = 100  # diffusivity
-    Lx = 300 # domain size
+    Lx = domain_size # domain size
     dx = 0.5
     C_left = 500
     C_right = 0
-    nt = 5000
+    nt = n_time_steps
 
     x, nx = make_grid(0, Lx, dx)
     dt = calculate_time_step(dx, D)
     C = set_initial_profile(nx, boundary_left=C_left, boundary_right=C_right)
 
-    plot_profile(x, C, title="Initial concentration profile")
+    if plot is True:
+        plot_profile(x, C, title="Initial concentration profile")
 
+    if plot is False:
+        print("Time = 0\n", C)
     for t in range(0, nt):
         solve_1d_diffusion(C, dx, dt, D)
+        if plot is False:
+            print(f"Time = {t*dt}\n", C)
 
-    plot_profile(x, C, color="b", title="Final concentration profile")
+    if plot is True:
+        plot_profile(x, C, color="b", title="Final concentration profile")
+
+
+if __name__ == "__main__":
+    print("Diffusion model")
+    diffusion_model(plot=False, domain_size=5, n_time_steps=5)
 
