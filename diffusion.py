edcf1ce72d65fbec795e8ad392549e3787fa73cb
Mark Piper <mark.piper@colorado.edu>
2025-01-09 17:08:28 +0000

    Complete modularization of model script

--- diffusion.py
+++ diffusion.py
@@ -6,36 +6,47 @@ def calculate_time_step(grid_spacing, diffusivity):
     return 0.5 * grid_spacing**2 / diffusivity
 
 
-D = 100  # diffusivity
-Lx = 300 # domain size
+def set_initial_profile(grid_size=100, boundary_left=500, boundary_right=0):
+    profile = np.empty(grid_size)
+    profile[: grid_size//2] = boundary_left
+    profile[grid_size//2 :] = boundary_right
+    return profile
 
-dx = 0.5
-x = np.arange(start=0, stop=Lx, step=dx)
-nx = len(x)
 
-C = np.zeros_like(x)
-C_left = 500
-C_right = 0
-C[x <= Lx//2] = C_left
-C[x > Lx//2] = C_right
+def make_grid(origin, domain_size, grid_spacing):
+    grid = np.arange(start=origin, stop=origin+domain_size, step=grid_spacing)
+    return (grid, len(grid))
 
-plt.figure()
-plt.plot(x, C, "r")
-plt.xlabel("x")
-plt.ylabel("C")
-plt.title("Initial concentration profile")
 
-time = 0
-nt = 5000
-dt = calculate_time_step(dx, D)
+def plot_profile(grid, concentration, color="r", title="Concentration profile"):
+    plt.figure()
+    plt.plot(grid, concentration, color)
+    plt.xlabel("x")
+    plt.ylabel("C")
+    plt.title(title)
 
-for t in range(0, nt):
-    C += D * dt / dx**2 * (np.roll(C, -1) - 2*C + np.roll(C, 1))
-    C[0] = C_left
-    C[-1] = C_right
 
-plt.figure()
-plt.plot(x, C, "b")
-plt.xlabel("x")
-plt.ylabel("C")
-plt.title("Final concentration profile")
+def solve_1d_diffusion(concentration, grid_spacing=1.0, time_step=1.0, diffusivity=1.0):
+    centered_difference = np.roll(concentration, -1) - 2*concentration + np.roll(concentration, 1)    
+    concentration[1:-1] += diffusivity * time_step / grid_spacing**2 * centered_difference[1:-1]
+
+
+def diffusion_model():
+    D = 100  # diffusivity
+    Lx = 300 # domain size
+    dx = 0.5
+    C_left = 500
+    C_right = 0
+    nt = 5000
+
+    x, nx = make_grid(0, Lx, dx)
+    dt = calculate_time_step(dx, D)
+    C = set_initial_profile(nx, boundary_left=C_left, boundary_right=C_right)
+
+    plot_profile(x, C, title="Initial concentration profile")
+
+    for t in range(0, nt):
+        solve_1d_diffusion(C, dx, dt, D)
+
+    plot_profile(x, C, color="b", title="Final concentration profile")
+
